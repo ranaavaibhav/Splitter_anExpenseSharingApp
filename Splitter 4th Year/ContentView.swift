08e3092e7629e92ed7597650a532388545b2821f
ranaavaibhav <145832975+ranaavaibhav@users.noreply.github.com>
2025-03-28 22:32:50 +0530

    Initial Commit

---
+++ Splitter 4th Year/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Splitter 4th Year
+//
+//  Created by Vaibhav on 28/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Splitter 4th Year/Splitter_4th_YearApp.swift
@@ -0,0 +1,17 @@
+//
+//  Splitter_4th_YearApp.swift
+//  Splitter 4th Year
+//
+//  Created by Vaibhav on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Splitter_4th_YearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
